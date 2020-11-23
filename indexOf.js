2fb3b410eee98a70544a7e3d1a2857c48b8c1737
Flynn Madsen <flynnisobelhugym.madsen@skola.botkyrka.se>
2020-11-23 15:33:55 +0100

    steg 1

--- indexOf.js
+++ indexOf.js
@@ -1,6 +1,13 @@
 
 function indexOf(arr, item) {
-    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === item) {
+            return arr[i]
+        }
+        
+    }
+    return 
+
 }
 
 const items = [

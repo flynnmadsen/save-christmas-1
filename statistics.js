a4c2bf0df10e2c07e9593e6c781551998120e768
Flynn Madsen <flynnisobelhugym.madsen@skola.botkyrka.se>
2020-11-30 14:52:48 +0100

    steg 2

--- statistics.js
+++ statistics.js
@@ -1,12 +1,35 @@
 
 function max(arr) {
+    let biggest 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > biggest) {
+            biggest = arr[i]
+        }
 
+    }
+    return biggest
 }
 
 function min(arr) {
-
+    let smallest 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] < biggest) {
+            smallest = arr[i]
+        }
+        
+    }
+    return smallest
 }
 
 function mean(arr) {
-    // Ledtråd: Börja med att räkna ut summan av alla element
+    let all
+    for (let i = 0; i < arr.length; i++) {
+        all = all + arr[i]
+        
+    }
+    return all / arr.length
 }
+
+console.log(max([1, 2, 3, 4, 5]))
+console.log(min([1, 2, 3, 4, 5]))
+console.log(mean([1, 2, 3, 4, 5]))
